419a190b7bce58b2f872fc50a406b57044fff557
Indunil1992 <indunil@adroitlogic.com>
2019-09-09 10:45:08 +0530

    dd

--- sachiiii/lambda.js
+++ sachiiii/lambda.js
@@ -1,4 +1,4 @@
-exports.handler = function (event, context, callback) {
-
-    callback(event);
+exports.handler =  async function(event, context) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return context.logStreamName
 }
